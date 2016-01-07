198861353a6409fd4c961c8513464a51a7073078
Jim Goodspeed <james.goodspeed@libertymutual.com>
2016-01-07 15:30:06 -0500

    Map route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('map', 'MapController@index') {
+
+}
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
