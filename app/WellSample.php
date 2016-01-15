90c21f417407376da8fd2a275b0b6f2095e825ee
Jim Goodspeed <jim@goodspeed.me>
2016-01-15 15:53:55 -0500

    Hard code table name

--- app/WellSample.php
+++ app/WellSample.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class WellSample extends Model
 {
 
+    public $table = "WellSample";
+
     // Which fields can be modified by users
     protected $fillable = [
         'sampleDate',
