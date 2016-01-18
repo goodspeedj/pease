85d8cf2b50ba95c5a9e49b7580bc2a0c44f5afb5
Jim Goodspeed <james.goodspeed@libertymutual.com>
2016-01-18 10:07:24 -0500

    Add Data Tables library

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ URL::asset('css/sticky-footer-navbar.css') }}">
+    @yield('custom_css')
   </head>
 
   <body>
@@ -57,5 +58,6 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    @yield('custom_js')
   </body>
 </html>
--- resources/views/pages/wellsample.blade.php
+++ resources/views/pages/wellsample.blade.php
@@ -2,10 +2,39 @@
 
 @section('title', 'Well Sample')
 
+@section('custom_css', '<link rel="stylesheet" href="https://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css">')
+
 @section('content')
     <p>Well Sample</p>
 
-    @foreach ($wellSamples as $wellSample)
-      <p>{{ $wellSample->pfcLevel }}</p>
-    @endforeach
+    <table id="table" class="display" cellspacing="0" width="100%">
+      <thead>
+        <th>Chemical</th>
+        <th>Date</th>
+        <th>PFC Level</th>
+      </thead>
+      <tbody>
+        @foreach ($wellSamples as $wellSample)
+          <tr>
+            <td>{{ $wellSample->chemID }}</td>
+            <td>{{ $wellSample->pfcLevel }}</td>
+            <td>{{ $wellSample->sampleDate }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    
+@stop
+
+@section('custom_js')
+
+    <script src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
+    
+    <script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+    </script>
+
 @stop
