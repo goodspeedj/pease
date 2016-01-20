908ad436c95b71c6ea72cdfb93aca0f219901591
Jim Goodspeed <james.goodspeed@libertymutual.com>
2016-01-20 12:49:37 -0500

    Add crosstab query as a query scope.

--- app/WellSample.php
+++ app/WellSample.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class WellSample extends Model
 {
@@ -21,8 +22,32 @@ class WellSample extends Model
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function checmical()
+    public function chemical()
     {
     	return $this->hasMany('App\Chemical');
+    }
+
+
+    /**
+     * Query to return well sample data in cross tab format
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCrosstab($query, $wellID)
+    {
+        //$wellID = 2;
+        return $query->select('sampleDate', \DB::raw("
+                    max(if(chemID=1, pfcLevel, ' ')) as 'PFOA', max(if(chemID=1, noteAbr, ' ')) as 'PFOANote',
+                    max(if(chemID=2, pfcLevel, ' ')) as 'PFOS', max(if(chemID=2, noteAbr, ' ')) as 'PFOSNote',
+                    max(if(chemID=3, pfcLevel, ' ')) as 'PFHxS', max(if(chemID=3, noteAbr, ' ')) as 'PFHxSNote',
+                    max(if(chemID=5, pfcLevel, ' ')) as 'PFOSA', max(if(chemID=5, noteAbr, ' ')) as 'PFOSANote',
+                    max(if(chemID=6, pfcLevel, ' ')) as 'PFNA', max(if(chemID=6, noteAbr, ' ')) as 'PFNANote',
+                    max(if(chemID=8, pfcLevel, ' ')) as 'PFPeA', max(if(chemID=8, noteAbr, ' ')) as 'PFPeANote',
+                    max(if(chemID=9, pfcLevel, ' ')) as 'PFHxA', max(if(chemID=9, noteAbr, ' ')) as 'PFHxANote',
+                    max(if(chemID=10, pfcLevel, ' ')) as 'PFBA', max(if(chemID=10, noteAbr, ' ')) as 'PFBANote'
+                "))
+            ->leftJoin('SampleNote', 'WellSample.noteID', '=', 'SampleNote.noteID')
+            ->where('wellID', '=', $wellID)
+            ->groupBy('sampleDate');
     }
 }
