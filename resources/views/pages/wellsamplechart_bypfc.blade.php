e7aedf3430705f5c7633b392330daf06cb4570d9
Jim Goodspeed <james.goodspeed@libertymutual.com>
2016-04-14 09:40:30 -0400

    Update chart title with PFC name

--- resources/views/pages/wellsamplechart_bypfc.blade.php
+++ resources/views/pages/wellsamplechart_bypfc.blade.php
@@ -45,52 +45,58 @@
 @stop
 
 @section('content')
-    <h3>Well Testing Samples</h3>
+
+    <h3 id="title">{{ $chem[0]->shortName }} Samples by Well</h3>
+    <h4>{{ $chem[0]->longName }}</h4>
+
 
     <div id="chart"></div>
 
     <script>
 
- //Raw data
- var data = <?php echo $wellSamples ?>;
- var dimensions = [
-    { key: 'Haven', value: 1 },
-    { key: 'Smith', value: 1 },
-    { key: 'Harrison', value: 1 },
-    { key: 'Collins', value: 0 },
-    { key: 'Portsmouth', value: 0 },
-    { key: 'WWTP', value: 1 },
-    { key: 'DES', value: 1 },
-    { key: 'GBK_PRE', value: 1 },
-    { key: 'GBK_POST1', value: 1 },
-    { key: 'GBK_POST2', value: 1 },
-    { key: 'DSC_PRE', value: 1 },
-    { key: 'DSC_POST', value: 1 },
-    { key: 'FIRESTATION', value: 0 },
-    { key: 'CSW-1D', value: 0 },
-    { key: 'CSW-1S', value: 0 },
-    { key: 'CSW-2R', value: 0 },
-    { key: 'HMW-3', value: 0 },
-    { key: 'HMW-8R', value: 0 },
-    { key: 'HMW-14', value: 0 },
-    { key: 'HMW-15', value: 0 },
-    { key: 'SMW-A', value: 0 },
-    { key: 'SMW-1', value: 0 },
-    { key: 'SMW-13', value: 0 },
-    { key: 'PSW-1', value: 0 },
-    { key: 'PSW-2', value: 0 }
-  ];
+       //Raw data
+       var data = <?php echo $wellSamples ?>;
 
- @include('partials.chartCommon')
- @include('partials.multiline')
+       var dimensions = [
+          { key: 'Haven', value: 1 },
+          { key: 'Smith', value: 1 },
+          { key: 'Harrison', value: 1 },
+          { key: 'Collins', value: 0 },
+          { key: 'Portsmouth', value: 0 },
+          { key: 'WWTP', value: 1 },
+          { key: 'DES', value: 1 },
+          { key: 'GBK_PRE', value: 1 },
+          { key: 'GBK_POST1', value: 1 },
+          { key: 'GBK_POST2', value: 1 },
+          { key: 'DSC_PRE', value: 1 },
+          { key: 'DSC_POST', value: 1 },
+          { key: 'FIRESTATION', value: 0 },
+          { key: 'CSW-1D', value: 0 },
+          { key: 'CSW-1S', value: 0 },
+          { key: 'CSW-2R', value: 0 },
+          { key: 'HMW-3', value: 0 },
+          { key: 'HMW-8R', value: 0 },
+          { key: 'HMW-14', value: 0 },
+          { key: 'HMW-15', value: 0 },
+          { key: 'SMW-A', value: 0 },
+          { key: 'SMW-1', value: 0 },
+          { key: 'SMW-13', value: 0 },
+          { key: 'PSW-1', value: 0 },
+          { key: 'PSW-2', value: 0 }
+        ];
 
- var chart = multilineChart()
-  .dimKey(function(d) { return d.wellName; })
-  .longDesc(function(d) {return d.wellDesc; })
-  .sortBy("wellID")
-  .dimensions(dimensions);
+       @include('partials.chartCommon')
+       @include('partials.multiline')
 
- d3.select("#chart").datum(data).call(chart);
+       
+
+       var chart = multilineChart()
+        .dimKey(function(d) { return d.wellName; })
+        .longDesc(function(d) {return d.wellDesc; })
+        .sortBy("wellID")
+        .dimensions(dimensions);
+
+       d3.select("#chart").datum(data).call(chart);
 
     </script>  
 @stop
