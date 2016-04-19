1730229b6798d7c6b71368e6115235071c7efcef
Jim Goodspeed <james.goodspeed@libertymutual.com>
2016-04-19 15:30:38 -0400

    Add home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Pease Well Water Contamination</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="css/main.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Pontano+Sans' rel='stylesheet' type='text/css'>
+
+    <style>
+      body { 
+        background: url(img/bg.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+        font-family: 'Pontano Sans', sans-serif;
+      }
+    </style>
+
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li><a href="/wellsample/Haven">Well Samples - Tables</a></li>
+                  <li><a href="/wellsample/well/haven/chart">Charts - by Well</a></li>
+                  <li><a href="/wellsample/well/haven/chart">Charts - by PFC</a></li>
+                  <li><a href="wellmap/pfoa">Map</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="push-right cover-heading">Pease Water</h1>
+            <p class="push-right cover-lead">In May 2014 it was discovered that one of the wells serving the Pease Tradeport in Portsmouth, NH was contaminated with perfluorochemicals (PFCs) at quantities above the EPAs Provisional Health Advisory level.</p>
+            <p class="push-right cover-lead">Before the well was shut down it is estimated that thousands of people were exposed, including children at two day care facilities, over a period of approximately 20 years.</p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+  </body>
+</html>
