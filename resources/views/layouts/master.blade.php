0b2635958d765538cb9162c53c23d6c4dee6a347
Jim Goodspeed <james.goodspeed@libertymutual.com>
2016-05-02 16:19:36 -0400

    Remove Home link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,7 +24,6 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="/">Home</a></li>
             <li><a href="/info">Info</a></li>
             <li class="dropdown">
               <a href="/wellsample/Haven" role="button" aria-haspopup="true" aria-expanded="false">Well Samples - Tables</a>
